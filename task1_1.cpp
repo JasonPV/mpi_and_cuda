4bac875239abef2c9adf7f80cc40345fbe892e43
Евгений <38925021+JasonPV@users.noreply.github.com>
2020-07-02 21:40:00 +0300

    Add files via upload

---
+++ task1_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "mpi.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int rank, size;
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	cout << "size: " << size << " rank: " << rank << endl;
+	MPI_Finalize();
+	return 0;
+}
